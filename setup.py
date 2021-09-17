4f5781d4da8ece779f3182a2b07f1332db82b580
franklinsijo <franklinsijo@gmail.com>
2021-09-18 01:53:03 +0530

    updated classifiers

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 from distutils.core import setup
+
 setup(
   name = 'csvviewer',     
   packages = ['csvviewer'],
@@ -8,19 +9,27 @@ setup(
   author = 'franklinsijo',
   author_email = 'franklinsijo@gmail.com', 
   url = 'https://github.com/franklinsijo/csvviewer', 
-  download_url = 'https://github.com/franklinsijo/csvviewer/archive/v_0.1.tar.gz', ##
+  download_url = 'https://github.com/franklinsijo/csvviewer/archive/v_0.1.tar.gz',
   keywords = ['csv', 'command-line', 'reader'], 
   install_requires=[        
           'prettytable',
       ],
   classifiers=[
     'Development Status :: 3 - Alpha', 
+    'Environment :: Console',
     'Intended Audience :: Developers',
     'Topic :: Software Development :: Build Tools',
     'License :: OSI Approved :: MIT License',   
+    'Natural Language :: English',
+    'Operating System :: MacOS :: MacOS X',
+    'Operating System :: POSIX',
+    'Operating System :: POSIX :: Linux',
     'Programming Language :: Python :: 3', 
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3.7',
+    'Programming Language :: Python :: 3.8',
+    'Programming Language :: Python :: 3.9'
   ],
 )
