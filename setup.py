a1edf2958fdcbfdacb045b99ae0f7ed6be1d679d
franklinsijo <franklinsijo@gmail.com>
2021-09-21 00:42:10 +0530

    version updated

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import setuptools
 setup(
   name = 'csvviewer',     
   packages = ['csvviewer'],
-  version = '0.1.0', 
+  version = '0.1.1', 
   license='MIT',  
   description = 'Command Line Delimited Text Viewer',
   author = 'franklinsijo',
